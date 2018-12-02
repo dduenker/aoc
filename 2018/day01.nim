d950bde36e9e7cbd8de9d25ab67160428d79905b
Daniel Dünker <dduenker@uni-koblenz.de>
2018-12-02 09:13:14 +0100

    improve code

--- 2018/day01.nim
+++ 2018/day01.nim
@@ -1,28 +1,18 @@
-import system,strutils,sets,os
-const input = splitLines(slurp("./input/01"))
+import strutils,sets,sequtils,math
+const
+        input = slurp("./input/01").strip().splitLines().map(parseInt)
 var 
         result = 0
         results=initSet[int]()
 
+echo input.sum()
 
-for i in 0..len(input)-1:
-        try:
-                result+=parseInt(input[i])
-        except ValueError:
-                break
-echo result
-
-result = 0
 while true:
         for i in 0..len(input)-1:
-                try:
-                        result+=parseInt(input[i])
-                        if result in results:
-                                echo result
-                                quit()
-                        else:
-                                incl(results, result)
-
-                except ValueError:
-                        break
+                result+=input[i]
+                if result in results:
+                        echo result
+                        quit()
+                else:
+                        incl(results, result)
 
