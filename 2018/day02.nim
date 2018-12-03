e27f91563f02a3cf343dd33c6705d38156f6b096
Daniel Dünker <dduenker@uni-koblenz.de>
2018-12-03 23:00:43 +0100

    solve part 2

--- 2018/day02.nim
+++ 2018/day02.nim
@@ -4,7 +4,21 @@ const
 var 
         twos = 0
         threes = 0
-        
+
+
+proc diff(x,y:string): int =
+        result=0
+        for i in 0..len(x)-1:
+                if x[i]!=y[i]:
+                        result=result+1
+
+proc diffpos(x,y:string): int =
+        for i in 0..len(x)-1:
+                if x[i]!=y[i]:
+                        result=i
+                        break
+
+
 for line in input:
         var data = initCountTable[char]()
         for c in line:
@@ -20,4 +34,14 @@ for line in input:
                         threes=threes+1
                         break
 echo twos*threes
+
+
+for i in 0..len(input)-2:
+        for j in i+1..len(input)-1:
+                if diff(input[i],input[j]) == 1:
+                        let p = diffpos(input[i],input[j])
+                        var solution=input[i]
+                        strutils.delete(solution, p, p)
+                        echo solution
+                        quit()
 quit()
